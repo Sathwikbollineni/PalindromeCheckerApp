6af0f8c5d95f292fff089d30f0a40d10fdea94cd
sathwik <sathwikbollineni2006@gmail.com>
2026-02-27 10:46:26 +0530

    UC3: Print a Hardcoded Palindrome Result

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -22,6 +22,20 @@ public class PalindromeCheckerApp {
                 } else {
                     System.out.println(" is NOT a Palindrome? :false.");
 
+                            String original = "madam";   // Input string
+                            String reversed = "";        // To store reversed string
+
+                            // Reverse string using for loop
+                            for (int i = original.length() - 1; i >= 0; i--) {
+                                reversed = reversed + original.charAt(i);
+                            }
+
+                            // Compare original and reversed string
+                            if (original.equals(reversed)) {
+                                System.out.println("The string is a palindrome.");
+                            } else {
+                                System.out.println("The string is not a palindrome.");
+                            }
+                        }
+                    }
                 }
-            }
-        }
